52169a5a771082993350d09a665f6a5f92be9da6
unknown <abdelazizabboapi@gmail.com>
2024-01-12 00:33:28 +0100

    Updates Heading

--- app/components/Heading.tsx
+++ app/components/Heading.tsx
@@ -15,7 +15,7 @@ const Heading: React.FC<HeadingProps> = ({
 }) => {
   return ( 
     <div className={center ? 'text-center' : 'text-start'}>
-      <div className="text-2xl font-bold titlemessage">
+      <div className="text-2xl font-bold">
         {title}
       </div>
       <div className="font-light text-neutral-500 mt-2">
